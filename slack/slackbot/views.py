1a13ba0b0e9df80f78e9a6baf509c21e04a3d127
serg <schkurko@yandex.ru>
2019-12-13 13:37:38 +0300

    slack bot response

--- slack/slackbot/views.py
+++ slack/slackbot/views.py
@@ -15,45 +15,81 @@ def slack(request):
             # type = data_dict['url_verification']
             # token = 'snb408WDDGItzsJ9hagIfNHK'
         except KeyError:
-            return JsonResponse(data_dict)
+            token = data_dict['token']
+            team_id = data_dict['team_id']
+            api_app_id = data_dict['api_app_id']
+            event = data_dict['event']
+            type_event = event['type']
+            channel = event['channel']
+            user = event['user']
+            text = event['text']
+            ts = event['ts']
+            event_ts = event['event_ts']
+            channel_type = event['channel_type']
+            type_request = data_dict['type']
+            # authed_teams = data_dict['authed_teams']
+            event_id = data_dict['event_id']
+            event_time = data_dict['event_time']
+            response = {"token": token,
+                        "team_id": team_id,
+                        "api_app_id": api_app_id,
+                        "event": {"type": type_event,
+                                  "user": user,
+                                  "text": text,
+                                  "item": {"type": "message",
+                                           "channel": channel,
+                                           "ts": ts,
+                                           },
+                                  "reaction": "slightly_smiling_face",
+                                  "item_user": user,
+                                  "event_ts": event_ts,
+                                  "channel_type": channel_type,
+
+                                  },
+                        "type": type_request,
+                        "authed_users": user,
+                        "event_id": event_id,
+                        "event_time": event_time,
+                        }
+            return JsonResponse(response)
 
 
 post_request = {
-                "token": "one-long-verification-token",
-                "team_id": "T061EG9R6",
-                "api_app_id": "A0PNCHHK2",
-                "event": {
-                    "type": "message",
-                    "channel": "D024BE91L",
-                    "user": "U2147483697",
-                    "text": "Hello hello can you hear me?",
-                    "ts": "1355517523.000005",
-                    "event_ts": "1355517523.000005",
-                    "channel_type": "im"
-                },
-                "type": "event_callback",
-                "authed_teams": ["T061EG9R6"],
-                "event_id": "Ev0PV52K21",
-                "event_time": 1355517523
-                }
-response = {
-            "token": "z26uFbvR1xHJEdHE1OQiO6t8",
-            "team_id": "T061EG9RZ",
-            "api_app_id": "A0FFV41KK",
-            "event": {
-                    "type": "reaction_added",
-                    "user": "U061F1EUR",
-                    "item": {
-                            "type": "message",
-                            "channel": "C061EG9SL",
-                            "ts": "1464196127.000002"
-                            },
-                    "reaction": "slightly_smiling_face",
-                    "item_user": "U0M4RL1NY",
-                    "event_ts": "1465244570.336841"
-                     },
-            "type": "event_callback",
-            "authed_users": "U061F7AUR",
-            "event_id": "Ev9UQ52YNA",
-            "event_time": 1234567890
-            }
+    "token": "one-long-verification-token",
+    "team_id": "T061EG9R6",
+    "api_app_id": "A0PNCHHK2",
+    "event": {
+        "type": "message",
+        "channel": "D024BE91L",
+        "user": "U2147483697",
+        "text": "Hello hello can you hear me?",
+        "ts": "1355517523.000005",
+        "event_ts": "1355517523.000005",
+        "channel_type": "im"
+    },
+    "type": "event_callback",
+    "authed_teams": ["T061EG9R6"],
+    "event_id": "Ev0PV52K21",
+    "event_time": 1355517523
+}
+response_ = {
+    "token": "z26uFbvR1xHJEdHE1OQiO6t8",
+    "team_id": "T061EG9RZ",
+    "api_app_id": "A0FFV41KK",
+    "event": {
+        "type": "reaction_added",
+        "user": "U061F1EUR",
+        "item": {
+            "type": "message",
+            "channel": "C061EG9SL",
+            "ts": "1464196127.000002"
+        },
+        "reaction": "slightly_smiling_face",
+        "item_user": "U0M4RL1NY",
+        "event_ts": "1465244570.336841"
+    },
+    "type": "event_callback",
+    "authed_users": "U061F7AUR",
+    "event_id": "Ev9UQ52YNA",
+    "event_time": 1234567890
+}
